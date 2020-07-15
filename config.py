d86475463e9eb239ba6684273b82243a06c78a34
FossilBlade <32032862+FossilBlade@users.noreply.github.com>
2020-07-15 12:08:05 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
-ticker_2_monitor = 'ZUO'
+ticker_2_monitor = 'HUBS'
 
-monitor_url = "file:///home/raush/vsts_git/PyCharmProjects/TickerMonitor_Mich/test_files/test_table.html"
+monitor_url = "https://www.fool.com/premium/rule-breakers/"
+# FOR MY TESTING --  monitor_url = "http://www.faucetsandshowerheads.com/recommendation.htm"
 
-monitor_interval = 0.0
 
-username = 'raushan.xxxxx@example.com'
-password = 'xyz@1234'
+monitor_interval = 0.005
+
+username = 'mike@bbdstores.com'
+password = '*******'
 
 # Change below to False if you dont want to close the chrome bowser after the script has completed.
 # This might be handy to troubleshoot any issues.
 
-close_chrome_after_complete = True
+close_chrome_after_complete = False
 
 # time out for the page to load
 page_load_timeout = 45
@@ -19,17 +21,26 @@ page_load_timeout = 45
 
 ### TRADING APP CONFIG
 ## variable to control the interaction with trading app. if this is False, no trading app actions will be taken.
+
 enable_trading_app_actions = True
 
-ticker_input_text_box_coordinates = (39, 229)
+ticker_input_text_box_coordinates = (2675, 125)
+ticker_input_text_box_coordinates2 = (3700, 270)
+ticker_input_text_box_coordinates3 = (3710, 440)
+ticker_input_text_box_coordinates4 = (2820, 95)
+ticker_input_text_box_coordinates5 = (60, 100)
 
-# Pres enter automatically after the ticker has been put in teh text box
-post_paste_actions_or_txt = ['enter','add']
+# Presses enter automatically after the ticker has been pasted in the text box
+
+post_paste_actions_or_txt = ['enter']
+post_paste_actions_or_txt2 = ['prtsc']
+post_paste_actions_or_txt3 = ['enter']
+post_paste_actions_or_txt4 = ['enter']
 
 # DEBUG OPTION
 
 # See mouse moving so that the script execution can be verified for various clicks.
 # PLEASE NOTE THAT THIS WILL DELAY THE EXECUTION SO IT SHOULD ONLY BE USED FOR TESTING AND VERIFICATION
-track_mouse_movements = True
+track_mouse_movements = False
 
 
